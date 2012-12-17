0c51b8f756be603911d8f44ac45ee5acf8c8902c
Federico Zuppa <fzuppa@gmail.com>
2012-12-17 12:42:40 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapDeviseActiveadminInHeroku::Application.config.secret_token = '11c5e5a99279d9220fb78a6b4be008079b66029ee3374995407abfe09b240ce82da142853d4436c45365a7e2cf7b751972c83ff516e0d800d2a5382d36b5941f'
